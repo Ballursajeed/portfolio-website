136406c1b3a27bec6ce9e9d85983d32095a52e53
ballursajeed <ballursajeed@gmail.com>
2024-08-22 17:12:26 +0530

    created nav for mobile devices

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button"
 
 //components
 import Nav from "./Nav"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return <header className="py-8 xl:py-12 text-white bg-pink-50/20">
@@ -21,7 +22,9 @@ const Header = () => {
        </div>
         
    {/* mobile nav */}
-    <div className="xl:hidden">mobile nav</div>
+    <div className="xl:hidden">
+        <MobileNav />
+    </div>
    
     </div>
   </header>
--- components/StairEffect.jsx
+++ components/StairEffect.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence,motion } from "framer-motion"
+import { AnimatePresence,delay,motion } from "framer-motion"
 import { usePathname } from "next/navigation"
 
 //components
@@ -20,6 +20,14 @@ const StairEffect = () => {
         flex">
             <Stairs />
         </div>
+        <motion.div  className="h-screen w-screen fixed bg-primary top-0
+        pointer-events-none" initial={{opacity: 1}} animate={{opacity: 0, transition:
+            {
+                delay: 1,
+                duraion: 0.4,
+                ease: "easeInOut"
+            }
+        }}/>
       </div>
     </AnimatePresence>
     </>
